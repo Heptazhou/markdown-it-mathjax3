6698e21171f076a72ddc2997b6ecf46f73862321
Heptazhou <zhou@0h7z.com>
2026-06-26 05:31:27 +0000

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { AllPackages } from "mathjax-full/js/input/tex/AllPackages.js"
-import { AssistiveMmlHandler } from "mathjax-full/js/a11y/assistive-mml.js"
-import { liteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor.js"
-import { mathjax } from "mathjax-full/js/mathjax.js"
-import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html.js"
-import { SVG } from "mathjax-full/js/output/svg.js"
-import { TeX } from "mathjax-full/js/input/tex.js"
-import juice from "juice/client"
+import { AssistiveMmlHandler } from "@mathjax/src/js/a11y/assistive-mml.js"
+import { liteAdaptor } from "@mathjax/src/js/adaptors/liteAdaptor.js"
+import { mathjax } from "@mathjax/src/js/mathjax.js"
+import { RegisterHTMLHandler } from "@mathjax/src/js/handlers/html.js"
+import { SVG } from "@mathjax/src/js/output/svg.js"
+import { TeX } from "@mathjax/src/js/input/tex.js"
+import juice from "juice"
 import type MarkdownIt from "markdown-it"
-import type StateBlock from "markdown-it/lib/rules_block/state_block"
-import type StateInline from "markdown-it/lib/rules_inline/state_inline"
-import type Token from "markdown-it/lib/token"
+import type StateBlock from "markdown-it/lib/rules_block/state_block.mjs"
+import type StateInline from "markdown-it/lib/rules_inline/state_inline.mjs"
+import type Token from "markdown-it/lib/token.mjs"
+
+import "@mathjax/src/js/util/asyncLoad/esm.js"
+
+const AllPackages = ["base", "ams", "mhchem", "newcommand", "noundefined"]
+import "@mathjax/src/js/input/tex/ams/AmsConfiguration.js"
+import "@mathjax/src/js/input/tex/base/BaseConfiguration.js"
+import "@mathjax/src/js/input/tex/mhchem/MhchemConfiguration.js"
+import "@mathjax/src/js/input/tex/newcommand/NewcommandConfiguration.js"
+import "@mathjax/src/js/input/tex/noundefined/NoUndefinedConfiguration.js"
 
 interface DocumentOptions {
 	InputJax: TeX<unknown, unknown, unknown>
@@ -26,8 +34,9 @@ function renderMath(content: string, documentOptions: DocumentOptions, convertOp
 	AssistiveMmlHandler(handler)
 	const mathDocument = mathjax.document(content, documentOptions)
 	const html = adaptor.outerHTML(mathDocument.convert(content, convertOptions))
-	const stylesheet = adaptor.outerHTML(documentOptions.OutputJax.styleSheet(mathDocument) as any)
-	return juice(html + stylesheet)
+	const css = adaptor.outerHTML(documentOptions.OutputJax.styleSheet(mathDocument) as any)
+	const ret = juice(css + html, { preserveFontFaces: false, preserveMediaQueries: false })
+	return ret
 }
 
 // Test if potential opening or closing delimieter
@@ -40,8 +49,8 @@ function isValidDelim(state: StateInline, pos: number) {
 	const prevChar = pos > 0 ? state.src.charCodeAt(pos - 1) : -1,
 		nextChar = pos + 1 <= max ? state.src.charCodeAt(pos + 1) : -1
 
-	// Check non-whitespace conditions for opening and closing, and
-	// check that closing delimeter isn't followed by a number
+	// Check non-whitespace conditions for opening and closing,
+	// and check that closing delimeter isn't followed by a number
 	if (prevChar === 0x20 /* " " */ || prevChar === 0x09 /* \t */ || (nextChar >= 0x30 /* "0" */ && nextChar <= 0x39) /* "9" */) {
 		can_close = false
 	}
@@ -70,14 +79,12 @@ function math_inline(state: StateInline, silent: boolean) {
 	}
 
 	// First check for and bypass all properly escaped delimieters
-	// This loop will assume that the first leading backtick can not
-	// be the first character in state.src, which is known since
-	// we have found an opening delimieter already.
+	// This loop will assume that the first leading backtick can not be the first character in state.src,
+	// which is known since we have found an opening delimieter already.
 	const start = state.pos + 1
 	let match = start
 	while ((match = state.src.indexOf("$", match)) !== -1) {
-		// Found potential $, look for escapes, pos will point to
-		// first non escape when complete
+		// Found potential $, look for escapes, pos will point to first non escape when complete
 		let pos = match - 1
 		while (state.src[pos] === "\\") {
 			pos -= 1
@@ -131,8 +138,8 @@ function math_inline(state: StateInline, silent: boolean) {
 function math_block(state: StateBlock, start: number, end: number, silent: boolean) {
 	let next: number, lastPos: number
 	let found = false,
-		pos = state.bMarks[start] + state.tShift[start],
-		max = state.eMarks[start],
+		pos = state.bMarks[start]! + state.tShift[start]!,
+		max = state.eMarks[start]!,
 		lastLine = ""
 
 	if (pos + 2 > max) {
@@ -161,10 +168,10 @@ function math_block(state: StateBlock, start: number, end: number, silent: boole
 			break
 		}
 
-		pos = state.bMarks[next] + state.tShift[next]
-		max = state.eMarks[next]
+		pos = state.bMarks[next]! + state.tShift[next]!
+		max = state.eMarks[next]!
 
-		if (pos < max && state.tShift[next] < state.blkIndent) {
+		if (pos < max && state.tShift[next]! < state.blkIndent) {
 			// non-empty line with negative indent should stop the list:
 			break
 		}
@@ -180,7 +187,7 @@ function math_block(state: StateBlock, start: number, end: number, silent: boole
 
 	const token = state.push("math_block", "math", 0)
 	token.block = true
-	token.content = (firstLine && firstLine.trim() ? firstLine + "\n" : "") + state.getLines(start + 1, next, state.tShift[start], true) + (lastLine && lastLine.trim() ? lastLine : "")
+	token.content = (firstLine && firstLine.trim() ? firstLine + "\n" : "") + state.getLines(start + 1, next, state.tShift[start]!, true) + (lastLine && lastLine.trim() ? lastLine : "")
 	token.map = [start, state.line]
 	token.markup = "$$"
 	return true
@@ -191,7 +198,7 @@ function plugin(md: MarkdownIt, options: any) {
 
 	const documentOptions = {
 		InputJax: new TeX({ packages: AllPackages, ...options?.tex }),
-		OutputJax: new SVG({ fontCache: "none", ...options?.svg }),
+		OutputJax: new SVG({ font: "mathjax-tex", ...options?.svg }),
 	}
 	const convertOptions = {
 		display: false,
@@ -202,15 +209,14 @@ function plugin(md: MarkdownIt, options: any) {
 	md.block.ruler.after("blockquote", "math_block", math_block, {
 		alt: ["paragraph", "reference", "blockquote", "list"],
 	})
-	md.renderer.rules.math_inline = function (tokens: Token[], idx: number) {
+	md.renderer.rules["math_inline"] = function (tokens: Token[], idx: number) {
 		convertOptions.display = false
-		return renderMath(tokens[idx].content, documentOptions, convertOptions)
+		return renderMath(tokens[idx]!.content, documentOptions, convertOptions)
 	}
-	md.renderer.rules.math_block = function (tokens: Token[], idx: number) {
+	md.renderer.rules["math_block"] = function (tokens: Token[], idx: number) {
 		convertOptions.display = true
-		return renderMath(tokens[idx].content, documentOptions, convertOptions)
+		return renderMath(tokens[idx]!.content, documentOptions, convertOptions)
 	}
 }
 
-plugin.default = plugin
-export = plugin
+export default plugin
